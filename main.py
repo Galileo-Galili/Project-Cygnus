550530f43a0d86beeb88e175259b9969ab512951
Naish Abbas <108818287+Naish031@users.noreply.github.com>
2023-04-05 00:46:49 +0500

    Pause function plus timer function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+import sys
+from button import Button
 
 
 pygame.init()
@@ -13,102 +15,107 @@ pygame.display.set_caption("Recycle Rush")
 # Variables
 WHITE = (255, 255, 255)
 FPS = 60
+
+# Start time for timer
+start_time = pygame.time.get_ticks()
+
 RECYCLE_BIN_WIDTH, RECYCLE_BIN_HEIGHT = (150, 250)
 TRASH_ITEMS_WIDTH, TRASH_ITEMS_HEIGHT = (70, 70)
-SCROLL_SPEED = 2 # pixels per frame
-TRASH_ITEMS_INTERVAL = 60 # frames between generating trash items
-TRASH_ITEMS_LIST = [] # list of trash items currently on the screen
-TRASH_ITEM_COUNTER = 0 # counter for generating new trash items
-SCROLL = 0
+TRASH_ITEMS_SPEED = 1  # pixels per frame
+TRASH_ITEMS_INTERVAL = 120  # frames between generating trash items
+trash_items_list = []  # list of trash items currently on the screen
+trash_items_counter = 0  # counter for generating new trash items
 
 
 # Top Screen Interface
+font = pygame.font.Font(None, 36)  # Font for text
 # Lifes
-def lives():
-    LIFE_IMAGE_1 = pygame.image.load("heart.png")
-    LIFE_1 = pygame.transform.scale(LIFE_IMAGE_1, (30, 30))  
-    LIFE_IMAGE_2 = pygame.image.load("heart.png")
-    LIFE_2 = pygame.transform.scale(LIFE_IMAGE_2, (30, 30))  
-    LIFE_IMAGE_3 = pygame.image.load("heart.png")
-    LIFE_3 = pygame.transform.scale(LIFE_IMAGE_3, (30, 30))  
+LIFE_IMAGE_1 = pygame.image.load("Assets/heart.png")
+LIFE_1 = pygame.transform.scale(LIFE_IMAGE_1, (30, 30))
+LIFE_IMAGE_2 = pygame.image.load("Assets/heart.png")
+LIFE_2 = pygame.transform.scale(LIFE_IMAGE_2, (30, 30))
+LIFE_IMAGE_3 = pygame.image.load("Assets/heart.png")
+LIFE_3 = pygame.transform.scale(LIFE_IMAGE_3, (30, 30))
 
-    SCREEN.blit(LIFE_1, (10, 10))
-    SCREEN.blit(LIFE_2, (45, 10))
-    SCREEN.blit(LIFE_3, (80, 10))
+# Pause Button
+PAUSE_IMAGE = pygame.image.load("Assets/pause.png")
+PAUSE = pygame.transform.scale(PAUSE_IMAGE, (30, 30))
+pause_rect = PAUSE.get_rect()
+pause_rect.top = 0
+pause_rect.right = 480
 
-# Pause Screen
-def pause_screen():
-    PAUSE_IMAGE = pygame.image.load("pause.png")
-    PAUSE = pygame.transform.scale(PAUSE_IMAGE, (30, 30)) 
+# 3 Recycle Bins
+PAPER_BIN_IMAGE = pygame.image.load("Assets/paper.png")
+PAPER_BIN = pygame.transform.scale(
+    PAPER_BIN_IMAGE, (RECYCLE_BIN_WIDTH, RECYCLE_BIN_HEIGHT))
+PLASTIC_BIN_IMAGE = pygame.image.load("Assets/plastic.png")
+PLASTIC_BIN = pygame.transform.scale(
+    PLASTIC_BIN_IMAGE, (RECYCLE_BIN_WIDTH, RECYCLE_BIN_HEIGHT))
+GLASS_BIN_IMAGE = pygame.image.load("Assets/glass.png")
+GLASS_BIN = pygame.transform.scale(
+    GLASS_BIN_IMAGE, (RECYCLE_BIN_WIDTH, RECYCLE_BIN_HEIGHT))
 
-    SCREEN.blit(PAUSE, (440, 10))
+# Trash Items
+PAPER_TRASH_IMAGE = pygame.image.load('Assets/paperTrash.png')
+PAPER_TRASH = pygame.transform.scale(
+    PAPER_TRASH_IMAGE, (TRASH_ITEMS_WIDTH, TRASH_ITEMS_HEIGHT))
 
-# Three Recycle Bins
-def recycle_bins():
-    PAPER_BIN_IMAGE = pygame.image.load("paper.png")
-    PAPER_BIN = pygame.transform.scale(PAPER_BIN_IMAGE, (RECYCLE_BIN_WIDTH, RECYCLE_BIN_HEIGHT))
-    PLASTIC_BIN_IMAGE = pygame.image.load("plastic.png")
-    PLASTIC_BIN = pygame.transform.scale(PLASTIC_BIN_IMAGE, (RECYCLE_BIN_WIDTH, RECYCLE_BIN_HEIGHT))
-    GLASS_BIN_IMAGE = pygame.image.load("glass.png")
-    GLASS_BIN = pygame.transform.scale(GLASS_BIN_IMAGE, (RECYCLE_BIN_WIDTH, RECYCLE_BIN_HEIGHT))
+PLASTIC_TRASH_IMAGE = pygame.image.load('Assets/plasticTrash.png')
+PLASTIC_TRASH = pygame.transform.scale(
+    PLASTIC_TRASH_IMAGE, (TRASH_ITEMS_WIDTH, TRASH_ITEMS_HEIGHT))
 
-    SCREEN.blit(PAPER_BIN, (10, 540))
-    SCREEN.blit(PLASTIC_BIN, (170, 540))
-    SCREEN.blit(GLASS_BIN, (320, 540))
+GLASS_TRASH_IMAGE = pygame.image.load('Assets/glassTrash.png')
+GLASS_TRASH = pygame.transform.scale(
+    GLASS_TRASH_IMAGE, (TRASH_ITEMS_WIDTH, TRASH_ITEMS_HEIGHT))
 
 
-# Trash items and moving trash items along the belt
+# Conveyor Belt
+CONVEYOR_BELT_IMAGE = pygame.image.load('Assets/conveyor3.png')
+CONVEYOR_BELT = pygame.transform.scale(CONVEYOR_BELT_IMAGE, (600, 50))
+conveyor_belt_width = CONVEYOR_BELT.get_width()
+SCROLL = 0
+
+# MOVING TRASH ITEMS
+
+
 def trash_items():
-
-    PAPER_TRASH_IMAGE = pygame.image.load('paperTrash.png')
-    PAPER_TRASH = pygame.transform.scale(PAPER_TRASH_IMAGE, (TRASH_ITEMS_WIDTH, TRASH_ITEMS_HEIGHT))
-
-    PLASTIC_TRASH_IMAGE = pygame.image.load('plasticTrash.png')
-    PLASTIC_TRASH = pygame.transform.scale(PLASTIC_TRASH_IMAGE, (TRASH_ITEMS_WIDTH, TRASH_ITEMS_HEIGHT))
-
-    GLASS_TRASH_IMAGE = pygame.image.load('glassTrash.png')
-    GLASS_TRASH = pygame.transform.scale(GLASS_TRASH_IMAGE, (TRASH_ITEMS_WIDTH, TRASH_ITEMS_HEIGHT))
-    
-    global TRASH_ITEMS_LIST, TRASH_ITEM_COUNTER
-    TRASH_ITEM_COUNTER += 1
-    if TRASH_ITEM_COUNTER >= TRASH_ITEMS_INTERVAL:
+    global trash_items_list, trash_items_counter
+    trash_items_counter += 1
+    if trash_items_counter >= TRASH_ITEMS_INTERVAL:
         # generate a new trash item
-        x = -TRASH_ITEMS_WIDTH # start offscreen to the left
-        y = 278 # y position on the conveyor belt
-        
+        x = -TRASH_ITEMS_WIDTH  # start offscreen to the left
+        y = 278  # y position on the conveyor belt
+
         trash_type = random.choice(['paper', 'plastic', 'glass'])
-        
+
         if trash_type == 'paper':
             trash_image = PAPER_TRASH
         elif trash_type == 'plastic':
             trash_image = PLASTIC_TRASH
         else:
             trash_image = GLASS_TRASH
-        TRASH_ITEMS_LIST.append({
+        trash_items_list.append({
             'image': trash_image,
             'rect': pygame.Rect(x, y, TRASH_ITEMS_WIDTH, TRASH_ITEMS_HEIGHT),
             'type': trash_type
         })
-        TRASH_ITEM_COUNTER = 0
+        trash_items_counter = 0
 
     # update position of each trash item
-    for trash_item in TRASH_ITEMS_LIST:
-        trash_item['rect'].x += SCROLL_SPEED
+    for trash_item in trash_items_list:
+        trash_item['rect'].x += TRASH_ITEMS_SPEED
 
     # remove trash items that have gone offscreen
-    TRASH_ITEMS_LIST = [trash_item for trash_item in TRASH_ITEMS_LIST if trash_item['rect'].right > 0]
+    trash_items_list = [
+        trash_item for trash_item in trash_items_list if trash_item['rect'].right > 0]
 
     # blit each trash item onto the screen
-    for trash_item in TRASH_ITEMS_LIST:
+    for trash_item in trash_items_list:
         SCREEN.blit(trash_item['image'], trash_item['rect'])
 
-
-# Conveyor Belt
-CONVEYOR_BELT_IMAGE = pygame.image.load('conveyor3.png')
-CONVEYOR_BELT = pygame.transform.scale(CONVEYOR_BELT_IMAGE, (600, 50))
-conveyor_belt_width = CONVEYOR_BELT.get_width()
-
 # Moving Conveyor belt
+
+
 def conveyor_belt():
     global SCROLL
     rel_x = SCROLL % CONVEYOR_BELT.get_rect().width
@@ -116,25 +123,90 @@ def conveyor_belt():
 
     if rel_x < WIDTH:
         SCREEN.blit(CONVEYOR_BELT, (rel_x, 350))
-    SCROLL += SCROLL_SPEED
+    SCROLL += 1
+
+
+def get_font(size):  # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("Assets/font.ttf", size)
+
+
+def paused():
+    clicked = True
+
+    PAUSE_MOUSE_POS = pygame.mouse.get_pos()
+    while clicked:
+        SCREEN.fill((255, 255, 255))
+        PAUSE_TEXT = get_font(45).render("PAUSED", True, "Black")
+        PAUSE_RECT = PAUSE_TEXT.get_rect(center=(WIDTH/2, 150))
+        SCREEN.blit(PAUSE_TEXT, PAUSE_RECT)
+
+        RESUME_BUTTON = Button(image=pygame.transform.scale(pygame.image.load("Assets/Play Rect.png"), (300, 75)), pos=(WIDTH//2, 275),
+                               text_input="RESUME", font=get_font(30), base_color="#ffffff", hovering_color="#000000")
+        RESTART_BUTTON = Button(image=pygame.transform.scale(pygame.image.load("Assets/Options Rect.png"), (300, 75)), pos=(WIDTH//2, 400),
+                                text_input="RESTART", font=get_font(30), base_color="#ffffff", hovering_color="#000000")
+        QUIT_BUTTON = Button(image=pygame.transform.scale(pygame.image.load("Assets/Quit Rect.png"), (300, 75)), pos=(WIDTH//2, 525),
+                             text_input="QUIT", font=get_font(30), base_color="#ffffff", hovering_color="#000000")
+
+        for button in [RESUME_BUTTON, RESTART_BUTTON, QUIT_BUTTON]:
+            button.changeColor(PAUSE_MOUSE_POS)
+            button.update(SCREEN)
+
+        PAUSE_BACK = Button(image=None, pos=(850, 460),
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+        PAUSE_BACK.changeColor(PAUSE_MOUSE_POS)
+        PAUSE_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PAUSE_BACK.checkForInput(PAUSE_MOUSE_POS):
+                    play()
+                # if PLAY_BUTTON.checkForInput(PAUSE_MOUSE_POS):
+                #     play()
+                # if OPTIONS_BUTTON.checkForInput(PAUSE_MOUSE_POS):
+                #     play()
+                # if QUIT_BUTTON.checkForInput(PAUSE_MOUSE_POS):
+                #     pygame.quit()
+                #     sys.exit()
+
+        pygame.display.update()
+
+
+def timer():
+    global timer_text
+
+    # Blit the timer text onto the screen at position (10, 10)
+    elapsed_time_seconds = (pygame.time.get_ticks() - start_time) // 1000
+    elapsed_time_minutes = elapsed_time_seconds // 60
+    elapsed_time_seconds %= 60
+
+    timer_text = font.render(
+        f"{elapsed_time_minutes:02}:{elapsed_time_seconds:02}", True, (0, 0, 0))
 
 
 def draw_window():
-    
-    SCREEN.fill(WHITE)
-    pause_screen()
-    lives()
-    recycle_bins() 
+    SCREEN.fill((255, 255, 0))
+    SCREEN.blit(PAUSE, (440, 10))
+    SCREEN.blit(LIFE_1, (WIDTH/2 - 40, 10))
+    SCREEN.blit(LIFE_2, (WIDTH/2 - 10, 10))
+    SCREEN.blit(LIFE_3, (WIDTH/2 + 20, 10))
+    SCREEN.blit(PAPER_BIN, (10, 540))
+    SCREEN.blit(PLASTIC_BIN, (170, 540))
+    SCREEN.blit(GLASS_BIN, (320, 540))
+    SCREEN.blit(timer_text, (10, 15))
     trash_items()
     conveyor_belt()
-
+    pygame.draw.rect(SCREEN, (0, 0, 0), (6, 10, 70, 30), width=2)
     pygame.display.update()
-    
 
 
-# Main Loop
-def main():
+def play():
     clock = pygame.time.Clock()
+
+    # PAUSE_BUTTON = Button(image=pygame.image.load("Assets/pause.png"), pos=(440, 10),
+    #                       text_input="PAUSE", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
     # Writing the run state
     run = True
     while run:
@@ -142,11 +214,20 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if pause_rect.collidepoint(event.pos):
+                    print("Image Clicked")
+                    paused()
+
+            # SCREEN.blit()
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     if PAUSE_BUTTON.checkForInput(pygame.mouse.get_pos):
+            #         paused()
+        timer()
         draw_window()
 
     pygame.quit()
 
 
 if __name__ == "__main__":
-    main()
+    play()
